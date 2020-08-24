16b685a73a962a9f49cb8376f64dfdda47f6ed47
evan999 <emeshberge@gmail.com>
2020-08-24 13:23:26 -0400

    Added in JWT configuration and services.

--- src/main/java/com/finance/frauddetector/authenticationms/Model/User.java
+++ src/main/java/com/finance/frauddetector/authenticationms/Model/User.java
@@ -7,7 +7,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
 
 public class User extends DateAudit {
     @Id
